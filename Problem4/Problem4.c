ba94e89ff8eab6c3f60c666bd82c23c6f694557a
Rahul Gupta <edc.rahul@gmail.com>
2018-03-08 17:51:28 -0500

    Update Problem4.c

--- Problem4/Problem4.c
+++ Problem4/Problem4.c
@@ -3,10 +3,28 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <string.h>
+#include <ctype.h>
 
-
+struct evaluate{
+	int int1;
+	char op1;
+	int int2;
+	char op2;
+	int int3;
+};
 
 int main()
 {
-
+	FILE *inputFile;
+	inputFile = fopen("inputFile.txt", "r");
+	if (inputFile == NULL){
+		printf("Error! File empty.\n");
+		return 0;
+	}
+	else {
+		fscanf(inputFile, "%d %c %d %c %d ", int1, op1, int2, op2, int3);
+	}
 }
