eae568af43e910a97db55a4e5a6428b25c0d860d
Jeremy Kritz <jaypanang@gmail.com>
2018-03-10 22:20:53 -0500

    works but calcs in parent

---
+++ Problem0/Problem0 - ours/parent0b.c
@@ -0,0 +1,111 @@
+﻿# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/wait.h>
+# include <limits.h>
+# include <time.h>
+
+int min = INT_MAX;
+int max = INT_MIN;
+
+FILE* f;
+FILE* output;
+
+int splitOff(int numKids, int index){
+    
+    pid_t pid;
+    int i;
+    f = fopen("data.txt", "r");
+    int status;
+
+    int curr, sum = 0;
+    output = fopen("output.txt", "w+");
+
+    printf("Initial process = %d\n", getpid());
+
+    int total = 0;
+    for (i = 0; i < numKids; i++)
+    {
+        total++;
+        if (fscanf(f, "%d", &curr) != EOF)
+        {
+            sum = sum + curr;
+        }
+        if (total < numKids)
+        {
+            pid = fork();
+        }
+        if (pid < 0)
+        {
+
+            printf("Error!\n");
+            return  -1;
+
+        }
+
+        else if (pid > 0)
+        {
+            i = numKids;
+            wait(NULL);
+           return 1;
+          
+        }
+
+        else if (pid == 0)
+        {
+
+            fprintf(output, "%d Hi I am process: %d and my parent is: %d\n", i, getpid(), getppid());
+
+            if (curr < min)
+            {
+                min = curr;
+            }
+            if (curr > max)
+            {
+                max = curr;
+            }
+            total = numKids;
+        }
+
+
+    }
+
+    fprintf(output, "\nmax: %d\n", max);
+    fprintf(output, "\nmin: %d\n", min);
+    fprintf(output, "\nsum: %d\n", sum);
+    exit(0);
+    return 0;
+}
+
+int main()
+{
+
+    double msec = 0, trigger = 10; /* 10ms */
+    clock_t first = clock();
+   
+
+
+        FILE* fi;
+        fi = fopen("data.txt", "r");
+        int listSize = 0;
+        int curr;
+        while (fscanf(fi, "%d", &curr) != EOF)
+        {
+            listSize++;
+        }
+    int isEnd = 0;
+    isEnd = splitOff(listSize, 0);
+ 
+   
+
+
+    clock_t timeElapsed = clock() - first;
+
+   
+    //x=1;
+    printf("Time: %.4f secs \n",(double)timeElapsed);
+
+
+    return 0;
+
+}
