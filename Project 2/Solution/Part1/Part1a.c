0665163816dfa363d0cdce69deb1b284a89bdbe1
Rahul Gupta <edc.rahul@gmail.com>
2018-04-18 21:29:03 -0400

    Add files via upload

--- Project 2/Solution/Part1/Part1a.c
+++ Project 2/Solution/Part1/Part1a.c
@@ -8,3 +8,42 @@
 #include <sys/types.h>
 #include <string.h>
 #include <stddef.h>
+
+FILE* writeF;
+
+typedef struct dataStruc { int min, max, sum, count; }
+dataStruc;
+
+void binarySplit(int lo, int hi, dataStruc* struc)
+{
+
+	if (lo == hi) // the end
+	{
+		int curr, num;
+		FILE* fp = fopen("data.txt", "r");
+
+		int cs = 0;
+		while (fscanf(fp, "%d\n", &num) != EOF)
+		{
+			if (lo == cs++)
+			{
+				curr = num;
+			}
+		}
+
+		if (curr < struc->min)
+		{
+			struc->min = curr;
+		}
+		if (curr > struc->max)
+		{
+			struc->max = curr;
+		}
+
+		struc->sum += curr;
+		struc->count++;
+		return;
+	}
+
+	int fd[2];
+
